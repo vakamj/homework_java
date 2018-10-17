2d9b31ef627cf5fcffdcb2bc2be6984aef87e452
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-10-17 16:27:42 +0600

    add psvm

---
+++ Main.java
@@ -0,0 +1,17 @@
+package it_sevenbits;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main (String args[]) throws IOException
+    {
+       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Parser class1 = new Parser();
+        String[] s = class1.parse(reader.readLine());
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
--- Parser.java
+++ Parser.java
@@ -1,33 +1,19 @@
 package it_sevenbits;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Parser<array> {
     public static String[] parse(final String strInput) {
-        char[] x = strInput.toCharArray();
-        int y = 0;
-        int n = 0;
+        char[] array1 = strInput.toCharArray();
+        int number_of_spaces = 0;
+        int dimension = 0;
         for (int i = 1; i < strInput.length(); i++) {
-            if (x[i] == ' ')
-                y++;
+            if (array1[i] == ' ')
+                number_of_spaces++;
         }
-        String[] array = new String[y+1];
+        String[] array = new String[number_of_spaces+1];
         for (String str : strInput.split(" ")) {
-            array[n] = str;
-            n++;
+            array[dimension] = str;
+            dimension++;
         }
         return array;
-    }
-    public static void main (String args[]) throws IOException
-    {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String str = reader.readLine();
-        Parser class1 = new Parser();
-        String[] s = class1.parse(reader.readLine());
-        for (int i = 0; i < s.length; i++) {
-            System.out.println(s[i]);
-        }
     }
 }
