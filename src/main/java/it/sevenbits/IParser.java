cbe5e7aab3b6f3e0d0977fffd0bcdf158b18249c
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-13 19:25:06 +0600

    Interface added

---
+++ src/main/java/it/sevenbits/IParser.java
@@ -0,0 +1,16 @@
+package it.sevenbits;
+
+import it.sevenbits.exceptions.ParserException;
+
+/**
+ * interface IParser
+ */
+public interface IParser {
+    /**
+     * @param delimiter delimiter
+     * @param source    source
+     * @return words from source
+     * @throws ParserException exceptions
+     */
+    String[] parse(String delimiter, String source) throws ParserException;
+}
