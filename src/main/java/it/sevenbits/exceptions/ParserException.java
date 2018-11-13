7473dc043600f24fa450619f10a754d5434d21ba
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-13 19:20:57 +0600

    Exception added

---
+++ src/main/java/it/sevenbits/exceptions/ParserException.java
@@ -0,0 +1,14 @@
+package it.sevenbits.exceptions;
+
+/**
+ * class ParserException
+ */
+public final class ParserException extends Exception {
+    /**
+     *
+     * @param message message
+     */
+    public ParserException(final String message) {
+        super(message);
+    }
+}
