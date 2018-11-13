2b29415e17ca98377a6de5a4b7c7c6a08a01d099
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-13 19:36:46 +0600

    pom file

---
+++ src/main/java/it/sevenbits/Parser.java
@@ -0,0 +1,24 @@
+package it.sevenbits;
+
+public class Parser {
+    public static String[] parse(final String strInput) {
+        int number = 0;
+        int j = 0;
+        int u = 0;
+        for (int i = 1; i < strInput.length(); i++) {
+            if (strInput.charAt(i) == ' ')
+                number++;
+        }
+        String[] array = new String[number + 1];
+        for (int i = 0; i < strInput.length(); i++) {
+            if (strInput.charAt(i) == ' ') {
+                array[u] = strInput.substring(j, i);
+                j = i + 1;
+                u++;
+            }
+
+        }
+        array[u] = strInput.substring(j);
+        return array;
+    }
+}
