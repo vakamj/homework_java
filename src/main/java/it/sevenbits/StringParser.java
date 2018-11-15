d54b2f30b33c962716e90491aa786d4e1219ec83
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-15 22:20:12 +0600

    Variables renamed, consecutive delimiters added

--- src/main/java/it/sevenbits/StringParser.java
+++ src/main/java/it/sevenbits/StringParser.java
@@ -7,34 +7,49 @@ import it.sevenbits.exceptions.ParserException;
  */
 public class StringParser implements IParser {
     /**
-     *
      * @param delimiter delimiter
-     * @param source source
+     * @param source    source
      * @return array
      * @throws ParserException exception
      */
-    public String[] parse(final String delimiter, final String source) throws  ParserException {
+    public String[] parse(final String delimiter, final String source) throws ParserException {
+        if (source == null) {
+            throw new ParserException("\n" +
+                    "null");
+        }
         if (source.length() == 0) {
             throw new ParserException("\n" +
                     "empty");
         }
-        int number = 0;
+
+        int delimitersAmount = 0;
         int j = 0;
-        int word = 0;
+        int wordCount = 0;
+        boolean delimsMet = true;
+
         for (int i = 1; i < source.length(); i++) {
-            if (source.charAt(i) == delimiter.charAt(0)) {
-                number++;
+            if ((source.charAt(i) == delimiter.charAt(0)) && (delimsMet)) {
+                delimsMet = false;
+                delimitersAmount++;
+            }
+            if ((source.charAt(i) != delimiter.charAt(0)) && (!delimsMet)) {
+                delimsMet = true;
             }
         }
-        String[] array = new String[number + 1];
+        String[] array = new String[delimitersAmount + 1];
         for (int i = 0; i < source.length(); i++) {
-            if (source.charAt(i) == delimiter.charAt(0)) {
-                array[word] = source.substring(j, i);
+            if ((source.charAt(i) == delimiter.charAt(0)) && (delimsMet)) {
+                delimsMet = false;
+                array[wordCount] = source.substring(j, i);
                 j = i + 1;
-                word++;
+                wordCount++;
+            }
+            if ((source.charAt(i) != delimiter.charAt(0)) && (!delimsMet)) {
+                j = i;
+                delimsMet = true;
             }
         }
-        array[word] = source.substring(j);
+        array[wordCount] = source.substring(j);
         return array;
     }
 }
