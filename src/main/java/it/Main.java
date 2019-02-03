ffe572aa1ae9710cf2770f49a8f89ce05e8582a8
Valya Myakisheva <myakisheva.59490@gmail.com>
2019-02-04 00:27:41 +0600

    program from console added

--- src/main/java/it/Main.java
+++ src/main/java/it/Main.java
@@ -1,6 +1,15 @@
 package it;
 
 
+import it.sevenbits.Grep.GrepFactory;
+import it.sevenbits.Grep.IGrep;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 /**
  * main
  */
@@ -14,8 +23,57 @@ public final class Main {
     /**
      * @param args standard input arguments
      */
-    public static void main(final String[] args)  {
+    public static void main(final String[] args) throws IOException {
+        // -s "one two three" -one three
+        // -f "src/test/java/resources/textGrep.txt" -one three
 
+        final int sourceGrep = 0;
+        final int stringGrep = 1;
+        final int typeGrep = 2;
+        final int delimiterFilter = 3;
+        if ((args.length < 4) || (args[sourceGrep].isEmpty()) || (args[typeGrep].isEmpty())
+                || (args[delimiterFilter].isEmpty())) {
+            throw new IOException("Oops, something went wrong");
         }
+
+        String delimiter = " ";
+        if (args.length == 5) {
+            delimiter = args[delimiterFilter];
+        }
+        GrepFactory grepFactory = new GrepFactory();
+        List<String> grepList = new ArrayList<>();
+        Collections.addAll(grepList, args[delimiterFilter].split(delimiter));
+        List<String> list;
+        IGrep grep;
+        Reader reader;
+        if (args[sourceGrep].equals("-f")) {
+            reader = new BufferedReader(new java.io.FileReader(new File(args[stringGrep])));
+
+        } else {
+            if (args[sourceGrep].equals("-s")) {
+                reader = new StringReader(args[stringGrep]);
+
+            } else {
+                throw new IOException("wrong source");
+            }
+        }
+        if (args[typeGrep].equals("-and")) {
+            grep = grepFactory.getAndGrep(grepList);
+        } else {
+            if (args[typeGrep].equals("-or")) {
+                grep = grepFactory.getOrGrep(grepList);
+            } else {
+
+                if (args[typeGrep].equals("-one")) {
+                    grep = grepFactory.getOneWordGrep(args[delimiterFilter]);
+                } else {
+                    throw new IOException("wrong typeGrep");
+                }
+            }
+        }
+        list = grep.doGrep(reader);
+        reader.close();
+        System.out.println(list.toString());
     }
+}
 
